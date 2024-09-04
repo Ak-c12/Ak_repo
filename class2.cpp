9ea99046bb0f6b3bc47e33ca766ec52e51a781d8
ak-c12 <guptaak85189@gmail.com>
2024-09-04 19:41:04 +0530

    first commit

---
+++ class2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"Enter the Character:"<< endl;
+    cin>>ch;
+    if(ch){
+        cout<<"this is lowercase"<< endl;
+    }
+    
+}
---
+++ first.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<" heii...I am learning C++" ;
+}
