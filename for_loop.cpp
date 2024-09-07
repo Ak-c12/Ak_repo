e64b01199c67f7d90930191d9071f886cb5d1f55
ak-c12 <guptaak85189@gmail.com>
2024-09-07 15:07:24 +0530

    printing the sum of n Numbers using "while loop"

--- for_loop.cpp
+++ for_loop.cpp
@@ -2,12 +2,23 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter the value of n: \n";
+    cout<<"Enter the value of n:";
     cin>>n;
-    int sum = 0;
+    int sum =0;
     for(int i=1;i<=n; i++){
         sum+=i;
     }
-    cout<<"Sum="<< sum<<endl;
+    
+    
+    // using for while loop
+    int n,i=1,sum=0;
+    cout<<"Enter the value of n:";
+    cin>>n;
+    while(i<=n){
+       sum+=i;
+       i++;
+    }
+
+    cout<<"Sum="<<sum<<endl;
     return 0;
 }
